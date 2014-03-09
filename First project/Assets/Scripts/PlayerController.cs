12a270353ff5eac4e0a05d897d8c8b4925ea4c2f
Seth Harper <sharper@fullsail.edu>
2014-03-08 21:15:11 -0500

    Added Accelerometer input for moving ship

--- First project/Assets/Scripts/PlayerController.cs
+++ First project/Assets/Scripts/PlayerController.cs
@@ -12,23 +12,65 @@ public class PlayerController : MonoBehaviour
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+	public float screenWidth = Screen.currentResolution.width;
+	public float screenHeight = Screen.currentResolution.height;
+
+	//public float speed = 10.0F;
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity = movement * speed;
+		Vector3 dir = Vector3.zero;
+		dir.x = Input.acceleration.x;
+		//dir.z = Input.acceleration.x;
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+			
+		dir *= Time.deltaTime;
+		transform.Translate(dir * speed);
 
 		rigidbody.position = new Vector3
-		(
-			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
-			0.0f, 
-			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-		);
-
+			(
+				Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+				0.0f, 
+				Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+				);
+		
 		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+
+
+		/*float moveHorizontal;
+		float moveVertical;
+		
+		if ((Input.touchCount == 1)) 
+		{
+			if ( Input.GetTouch(0).position.x < (screenWidth/2) )
+			{
+				moveHorizontal = -1;
+			}
+			else
+			{
+				moveHorizontal = 1;
+			}
+		}
+		else
+		{
+			moveHorizontal = 0;
+		}
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+		rigidbody.velocity = movement * speed * Time.deltaTime;
+		
+		//movement script goes here 
+		rigidbody.position = new Vector3
+			(
+				Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+				0.0f, 
+				Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+				);
+
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);*/
+
 		
 	}
 }
